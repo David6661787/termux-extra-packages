8d30692b8bc695745b3d7253ce1cdfa02a24dacd
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-21 18:59:57 +0800

    update cli-visualizer

--- packages/cli-visualizer/build.sh
+++ packages/cli-visualizer/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/dpayne/cli-visualizer
 TERMUX_PKG_DESCRIPTION="Command line visualizer."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@WMCB-Tech"
-TERMUX_PKG_VERSION=1.7
+TERMUX_PKG_VERSION=1.8
 TERMUX_PKG_SRCURL="https://github.com/dpayne/cli-visualizer/archive/refs/tags/v${TERMUX_PKG_VERSION}.zip"
-TERMUX_PKG_SHA256=5411f3a7baed7983fd9b4cfb52a224a91038d331d4d28bac718401d12d6830d8
+TERMUX_PKG_SHA256=feae8d9b9c5c44bc15df1617a1279162755b9ef43e6c57d9cc01c1e84efb213d
 # there is a package vis is a text editor. and is collided with cli-visualizer
 TERMUX_PKG_CONFLICTS="vis"
 TERMUX_PKG_DEPENDS="cmake, fftw, ncurses, pulseaudio"
