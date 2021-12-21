203c18ff3a9cc5b92260db2287359843b3c8f2ce
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-21 18:51:54 +0800

    fix dependency

--- packages/cli-visualizer/build.sh
+++ packages/cli-visualizer/build.sh
@@ -7,4 +7,4 @@ TERMUX_PKG_SRCURL="https://github.com/dpayne/cli-visualizer/archive/refs/tags/v$
 TERMUX_PKG_SHA256=5411f3a7baed7983fd9b4cfb52a224a91038d331d4d28bac718401d12d6830d8
 # there is a package vis is a text editor. and is collided with cli-visualizer
 TERMUX_PKG_CONFLICTS="vis"
-TERMUX_PKG_DEPENDS="cmake, fftw3, ncurses, pulseaudio"
+TERMUX_PKG_DEPENDS="cmake, fftw, ncurses, pulseaudio"
