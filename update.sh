f03e92fbbed4c25014f1ffe59ac7a51ea33ce12a
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-22 20:08:52 +0800

    hide x11/root repositories when cloning

--- update.sh
+++ update.sh
@@ -22,12 +22,12 @@ git clone https://github.com/termux/termux-packages
 # clone all repositories if this file renamed to update-all.sh
 if [ "$(basename $0)" == "update-all.sh" ] || [ "$(basename $0)" == "init-all-repos.sh" ]; then
 	# delete exising directories
-	rm -rf x11-packages
-	rm -rf termux-root-packages
+	rm -rf .x11-packages
+	rm -rf .root-packages
 
 	echo "[*] Cloning root/x11 packages...."
-	git clone https://github.com/termux/x11-packages
-	git clone https://github.com/termux/termux-root-packages
+	git clone https://github.com/termux/x11-packages .x11-packages
+	git clone https://github.com/termux/termux-root-packages .root-packages
 
 	for repos in x11-packages termux-root-packages; do
 		rm -rf ${repos}/.git
