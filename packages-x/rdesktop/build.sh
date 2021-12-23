99592855181e6ebc8ebc3b4276bb63531f926eea
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-23 21:54:00 +0800

    new package: rdesktop

---
+++ packages-x/rdesktop/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://rdesktop.org
+TERMUX_PKG_DESCRIPTION="rdesktop is an open source client for Microsoft's RDP protocol."
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@WMCB-Tech"
+TERMUX_PKG_VERSION=1.9.0
+TERMUX_PKG_SRCURL="https://github.com/rdesktop/rdesktop/releases/download/v${TERMUX_PKG_VERSION}/rdesktop-${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=473c2f312391379960efe41caad37852c59312bc8f100f9b5f26609ab5704288
+TERMUX_PKG_DEPENDS="iconv, libgnutls, libxrandr, libxcursor, libtasn1, libnettle, libx11, pulseaudio"
+
+
