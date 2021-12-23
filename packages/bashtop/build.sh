b2e4b0ff54dc4bdc37859122f6ca5147085ffba7
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-23 13:56:24 +0800

    restart build

--- packages/bashtop/build.sh
+++ packages/bashtop/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="Resource monitor that shows usage and stats for processo
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@WMCB-Tech"
 TERMUX_PKG_VERSION="0.9.25"
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/aristocratos/bashtop/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=676126df13a860df578a3a130783e0abb1c0b7628dddcfd7591069b832e8d8ad
 TERMUX_PKG_DEPENDS="bash, coreutils, gawk, procps, sed"
