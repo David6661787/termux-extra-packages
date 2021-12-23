6e53a7db52ce9dea3917a9d39cd9a2518e1de8ff
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-23 20:15:25 +0800

    jwm: fix sha256sum

--- packages-x/jwm/build.sh
+++ packages-x/jwm/build.sh
@@ -4,5 +4,5 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@WMCB-Tech"
 TERMUX_PKG_VERSION=2.4.0
 TERMUX_PKG_SRCURL="https://github.com/joewing/jwm/releases/download/v${TERMUX_PKG_VERSION}/jwm-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=c5338e2bbe11e6855734a89042d924009dc026a09fbe2992f5a6b3a85ab8c6ff
+TERMUX_PKG_SHA256=1449754f693bdfabbe6318803120722cc65197a73d6bf38120090aee18b16040
 TERMUX_PKG_DEPENDS="libcairo, libjpeg-turbo, libpng, libxext, libxrender, libxmu, libxinerama, libxpm, pango"
