0387b42252563d6719ba6f8c0e9934ded52ac61a
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-23 19:00:09 +0800

    new package: jwm

---
+++ packages-x/jwm/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://joewing.net/projects/jwm/
+TERMUX_PKG_DESCRIPTION="Very small lightweight pure X11 window manager with tray and menus"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@WMCB-Tech"
+TERMUX_PKG_VERSION=2.4.0
+TERMUX_PKG_SRCURL="https://github.com/joewing/jwm/releases/download/v${TERMUX_PKG_VERSION}/jwm-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256=c5338e2bbe11e6855734a89042d924009dc026a09fbe2992f5a6b3a85ab8c6ff
+TERMUX_PKG_DEPENDS="libcairo, libjpeg-turbo, libpng, libxext, libxrender, libxmu, libxinerama, libxpm, pango"
