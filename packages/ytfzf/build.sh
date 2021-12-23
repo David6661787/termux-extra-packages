23a50d03cd25f0c7e79dec4081911c82009ddbf3
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-23 14:57:27 +0800

    update ytfzf

--- packages/ytfzf/build.sh
+++ packages/ytfzf/build.sh
@@ -9,6 +9,7 @@ TERMUX_PKG_DEPENDS="fzf, jq"
 # since youtube-dl can't be easily installed. we have to tell users to install it
 # in case if a user specified --no-install-recommends we just simply tell users to install it
 TERMUX_PKG_RECOMMENDS="python, mpv"
+TERMUX_PKG_SUGGESTS="mpv-x"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
@@ -23,6 +24,13 @@ termux_step_create_debscripts(){
 		pip3 install youtube-dl
 	else
 		echo "You must install youtube-dl later in order to use this program"
+		echo "Run the following if you have python installed:"
+		echo
+		echo '     pip3 install youtube-dl'
 	fi
+
+	# message
+	echo
+	echo "To play videos, you must install 'mpv-x' in x11-repo"
 	EOT
 }
