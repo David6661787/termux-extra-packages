66b09d78fdef511b4c369c210e9d9a07f0abd0ae
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-24 16:52:42 +0800

    proot: update

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://proot-me.github.io/
 TERMUX_PKG_DESCRIPTION="Emulate chroot, bind mount and binfmt_misc for non-root users"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="Michal Bednarski @michalbednarski"
+TERMUX_PKG_MAINTAINER="@WMCB-Tech"
 # Just bump commit and version when needed:
 _COMMIT=7d6bdd9f6cf31144e11ce65648dab2a1e495a7de
 TERMUX_PKG_VERSION=5.1.107.custom
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=1bfdc2086e03a5b591b299d8314d5d519a367cb258b3d21414d928c75669f308
 TERMUX_PKG_DEPENDS="libtalloc"
