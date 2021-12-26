dd8e7ee6fd9e5a8036e68d0ba63d8e76be85a1fa
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-26 13:39:10 +0800

    proot, jwm: update packages

--- packages-x/jwm/build.sh
+++ packages-x/jwm/build.sh
@@ -3,7 +3,14 @@ TERMUX_PKG_DESCRIPTION="Very small lightweight pure X11 window manager with tray
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@WMCB-Tech"
 TERMUX_PKG_VERSION=2.4.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/joewing/jwm/releases/download/v${TERMUX_PKG_VERSION}/jwm-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=1449754f693bdfabbe6318803120722cc65197a73d6bf38120090aee18b16040
 TERMUX_PKG_DEPENDS="libcairo, libjpeg-turbo, libpng, libxext, libxrender, libxmu, libxinerama, libxpm, pango"
+TERMUX_PKG_SUGGESTS="audacious, galculator, pcmanfm, xorg-xclock"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_make_install(){
+	# install our jwmrc instead of default one
+	cp -a jwmrc $TERMUX_PREFIX/etc/system.jwmrc
+}
