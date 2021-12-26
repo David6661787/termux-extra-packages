4cf08c28fb6f1c9ec675b6fca9349b0d86b9e901
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-26 14:39:30 +0800

    jwm: fix install

--- packages-x/jwm/build.sh
+++ packages-x/jwm/build.sh
@@ -7,10 +7,10 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/joewing/jwm/releases/download/v${TERMUX_PKG_VERSION}/jwm-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=1449754f693bdfabbe6318803120722cc65197a73d6bf38120090aee18b16040
 TERMUX_PKG_DEPENDS="libcairo, libjpeg-turbo, libpng, libxext, libxrender, libxmu, libxinerama, libxpm, pango"
-TERMUX_PKG_SUGGESTS="audacious, galculator, pcmanfm, xorg-xclock"
+TERMUX_PKG_SUGGESTS="audacious, galculator, netsurf, pcmanfm, xorg-xclock"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install(){
 	# install our jwmrc instead of default one
-	cp -a jwmrc $TERMUX_PREFIX/etc/system.jwmrc
+	cp -a $TERMUX_PKG_BUILDER_DIR/jwmrc $TERMUX_PREFIX/etc/system.jwmrc
 }
